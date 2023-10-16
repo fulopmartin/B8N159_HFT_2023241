c36d84d74a2094b00776fb64a5e3789149496fbe
Fülöp Martin <fulopmartin1010@gmail.com>
2023-10-16 11:46:14 +0200

    Add project files.

---
+++ B8N159_HFT_2023241.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B8N159_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ B8N159_HFT_2023241.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace B8N159_HFT_2023241.Repository
+{
+    public class Class1
+    {
+    }
+}
