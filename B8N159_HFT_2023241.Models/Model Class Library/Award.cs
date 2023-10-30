59aa1ff2e10917f6a9bb91cef4bbc693d424e9f7
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-10-30 14:42:11 +0100

    Award osztály adattagok deklarálása

--- B8N159_HFT_2023241.Models/Model Class Library/Award.cs
+++ B8N159_HFT_2023241.Models/Model Class Library/Award.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace B8N159_HFT_2023241.Models
 {
     public class Award
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AwardId { get; set; }
+        public int AwardYear { get; set; }
+        public string AwardName { get; set; }
+
+        public virtual ICollection<Wine> Wines { get; set; }
     }
 }
