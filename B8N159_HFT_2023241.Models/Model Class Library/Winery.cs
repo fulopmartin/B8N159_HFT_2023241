788aa7d50b7b601aa823df453580015416e0b29b
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-10-30 12:57:12 +0100

    Winery osztály adattagok definiálása

--- B8N159_HFT_2023241.Models/Model Class Library/Winery.cs
+++ B8N159_HFT_2023241.Models/Model Class Library/Winery.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,18 @@ namespace B8N159_HFT_2023241.Models
 {
     public class Winery
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WineryId { get; set; }
+        public string Name { get; set; }
+        public int Zipcode { get; set; }
+        public virtual ICollection<Wine> Wines { get; set; }
+
+        public Winery()
+        {
+            this.Wines = new HashSet<Wine>();
+        }
+
     }
 }
