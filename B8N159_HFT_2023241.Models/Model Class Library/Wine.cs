b75fe980205350079bfa8728ce471f11edee4411
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-10-30 13:44:51 +0100

    Wine osztály adattagok deklarálása

--- B8N159_HFT_2023241.Models/Model Class Library/Wine.cs
+++ B8N159_HFT_2023241.Models/Model Class Library/Wine.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace B8N159_HFT_2023241.Models
 {
     public class Wine
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WineId { get; set; }
+
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+
+        public int WineryId { get; set; }
+        public int AwardId { get; set; }
+        
+        public virtual Winery Winery { get; private set; }
+        public virtual Award Award { get; private set; }
+        
     }
 }
