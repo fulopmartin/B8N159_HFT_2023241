d53f8bbbb06304b4ade0bbc3287605bd06653937
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-10-30 11:44:44 +0100

    Repository interfészek hozzáadva

---
+++ B8N159_HFT_2023241.Repository/Interfaces/IWineryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B8N159_HFT_2023241.Repository.Interfaces
+{
+    internal interface IWineryRepository
+    {
+    }
+}
