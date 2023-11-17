1a5eac561df17aaacc0e69c113dc5df15c0240b1
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-17 15:56:51 +0100

    Winery Repossitory deklarálása

--- B8N159_HFT_2023241.Repository/ModelRepositories/WineryRepository.cs
+++ B8N159_HFT_2023241.Repository/ModelRepositories/WineryRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using B8N159_HFT_2023241.Models;
+using B8N159_HFT_2023241.Repository.GenericRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace B8N159_HFT_2023241.Repository.ModelRepositories
 {
-    internal class WineryRepository
+    public class WineryRepository : Repository<Winery>, IRepository<Winery>
     {
+        public WineryRepository(WineryDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Winery Read(int id)
+        {
+            return ctx.Wineries.FirstOrDefault(t => t.WineryId == id);
+
+        }
+
+        public override void Update(Winery item)
+        {
+            var old = Read(item.WineryId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(item, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
     }
 }
