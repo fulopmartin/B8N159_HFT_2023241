737507710fba4f083e8afdb2ce06a6b43fd221a4
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-17 15:49:05 +0100

    AwardRepository deklarálás

--- B8N159_HFT_2023241.Repository/ModelRepositories/AwardRepository.cs
+++ B8N159_HFT_2023241.Repository/ModelRepositories/AwardRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using B8N159_HFT_2023241.Models;
+using B8N159_HFT_2023241.Repository.GenericRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace B8N159_HFT_2023241.Repository.ModelRepositories
 {
-    internal class AwardRepository
+    internal class AwardRepository : Repository<Award>, IRepository<Award>
     {
+        public AwardRepository(WineryDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Award Read(int id)
+        {
+            return ctx.Awards.FirstOrDefault(t => t.AwardId == id);
+        }
+
+        public override void Update(Award item)
+        {
+            var old = Read(item.AwardId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(item, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
     }
 }
