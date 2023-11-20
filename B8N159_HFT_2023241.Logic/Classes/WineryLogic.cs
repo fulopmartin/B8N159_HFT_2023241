c194ff7b9d5873f2e059e33ba0c05664120bc9cb
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-20 10:11:37 +0100

    Winery logic class implementálása

--- B8N159_HFT_2023241.Logic/Classes/WineryLogic.cs
+++ B8N159_HFT_2023241.Logic/Classes/WineryLogic.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using B8N159_HFT_2023241.Logic.Interfaces;
+using B8N159_HFT_2023241.Models;
+using B8N159_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace B8N159_HFT_2023241.Logic.Classes
 {
-    public class WineryLogic
+    public class WineryLogic : IWineryLogic
     {
+        IRepository<Winery> repo;
+
+        public WineryLogic(IRepository<Winery> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Winery item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Winery Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Winery> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Winery item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
