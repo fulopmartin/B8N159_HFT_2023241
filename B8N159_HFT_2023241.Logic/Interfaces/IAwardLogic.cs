76e103a4caf1f19516b1987f9f56bdcadc93a0d5
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-20 09:57:58 +0100

    Award logic interfész deklarálása

--- B8N159_HFT_2023241.Logic/Interfaces/IAwardLogic.cs
+++ B8N159_HFT_2023241.Logic/Interfaces/IAwardLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using B8N159_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace B8N159_HFT_2023241.Logic.Interfaces
 {
     public interface IAwardLogic
     {
+        void Create(Award item);
+        void Delete(int id);
+        Award Read(int id);
+        IQueryable<Award> ReadAll();
+        void Update(Award item);
     }
 }
