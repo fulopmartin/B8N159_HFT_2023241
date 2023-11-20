dcb69c78fbcf7cd359cf40d730ef4d99034e3f76
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-20 09:56:53 +0100

    Wine logic iterfész deklarálása

--- B8N159_HFT_2023241.Logic/Interfaces/IWineLogic.cs
+++ B8N159_HFT_2023241.Logic/Interfaces/IWineLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using B8N159_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace B8N159_HFT_2023241.Logic.Interfaces
 {
     public interface IWineLogic
     {
+        void Create(Wine item);
+        void Delete(int id);
+        Wine Read(int id);
+        IQueryable<Wine> ReadAll();
+        void Update(Wine item);
     }
 }
