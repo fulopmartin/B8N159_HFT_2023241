49ac2e3f449ddb5c16401bd05ce866d766a10e63
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-20 10:09:57 +0100

    Wine logic class implementálása

--- B8N159_HFT_2023241.Logic/Classes/WineLogic.cs
+++ B8N159_HFT_2023241.Logic/Classes/WineLogic.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using B8N159_HFT_2023241.Logic.Interfaces;
+using B8N159_HFT_2023241.Models;
+using B8N159_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace B8N159_HFT_2023241.Logic.Classes
 {
-    public class WineLogic
+    public class WineLogic : IWineLogic
     {
+        IRepository<Wine> repo;
+
+        public WineLogic(IRepository<Wine> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Wine item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Wine Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Wine> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Wine item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
