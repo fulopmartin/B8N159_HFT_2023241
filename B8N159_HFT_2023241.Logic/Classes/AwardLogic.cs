81e339070c9e437081461b842e9e1452a2e1eeae
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-20 10:07:55 +0100

    Award logic class implementálása

--- B8N159_HFT_2023241.Logic/Classes/AwardLogic.cs
+++ B8N159_HFT_2023241.Logic/Classes/AwardLogic.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using B8N159_HFT_2023241.Logic.Interfaces;
+using B8N159_HFT_2023241.Models;
+using B8N159_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace B8N159_HFT_2023241.Logic.Classes
 {
-    public class AwardLogic
+    public class AwardLogic : IAwardLogic
     {
+        IRepository<Award> repo;
+
+        public AwardLogic(IRepository<Award> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Award item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Award Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Award> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Award item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
