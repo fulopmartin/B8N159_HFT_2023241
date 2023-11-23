424b3d989fff863a0e60f8fc8d03048850b4f58d
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-23 15:38:31 +0100

    Rest service namespace beállítása

--- B8N159_HFT_2023241.Client/Program.cs
+++ B8N159_HFT_2023241.Client/Program.cs
@@ -1,7 +1,4 @@
-﻿using B8N159_HFT_2023241.Logic;
-using B8N159_HFT_2023241.Models;
-using B8N159_HFT_2023241.Repository;
-using B8N159_HFT_2023241.Repository.ModelRepositories;
+﻿using B8N159_HFT_2023241.Models;
 using System;
 
 namespace B8N159_HFT_2023241.Client
@@ -10,24 +7,7 @@ namespace B8N159_HFT_2023241.Client
     {
         static void Main(string[] args)
         {
-            WineryDbContext db = new WineryDbContext();
-
-            WineryRepository wr = new WineryRepository(db);
-            WineryLogic wl = new WineryLogic(wr);
-            WineRepository winer = new WineRepository(db);
-            WineLogic winel = new WineLogic(winer);
-            var avgByWinery =  wl.AveragePriceByWinery();
-
-            var avg = wl.AverageWinePrice();
-
-            var winesWhitoutAward = wl.WinesWhitoutAwardByWinery();
-
-            var winesWithNationalAward = winel.WinesWithNationalAward();
             
-            var WineryWithMostExpensiveWine = wl.WineryWithMostExpensiveWine();
-
-            var WineWithMostDomesticAwards = winel.WineWithMostDomesticAward();
-
             
         }
     }
--- B8N159_HFT_2023241.Client/RestServices.cs
+++ B8N159_HFT_2023241.Client/RestServices.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MovieDbApp.RestClient
+
+namespace B8N159_HFT_2023241.Client
 {
     class RestService
     {
