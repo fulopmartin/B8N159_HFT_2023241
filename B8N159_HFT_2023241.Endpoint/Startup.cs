527c5676d99fd3523dc58e43647d96001ad33c2c
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2023-11-24 22:52:35 +0100

    Apró módosítás usingban

--- B8N159_HFT_2023241.Endpoint/Startup.cs
+++ B8N159_HFT_2023241.Endpoint/Startup.cs
@@ -5,16 +5,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace B8N159_HFT_2023241.Endpoint
 {
