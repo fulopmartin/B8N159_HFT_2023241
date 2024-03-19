aebfca1db23d76d298c0dd2b5e7775371163511c
Fülöp Martin <fulopmartin1010@stud.uni-obuda.hu>
2024-03-19 09:22:56 +0100

    Wpf project added

---
+++ B8N159_HFT_2023241.GUI_Client/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace B8N159_HFT_2023241.GUI_Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
